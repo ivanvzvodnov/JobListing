ca0e07b70d5876f9b7d2fef63a479631cb51dc10
ivanvzvodnov <vzvodnov43@gmail.com>
2021-09-04 04:44:19 +0300

    Updates

---
+++ precache-manifest.b9cb7128e9cf48f92a9ded1f2cf0ba9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67139b20cd2d530135accb529dc872fc",
+    "url": "/JobListing/index.html"
+  },
+  {
+    "revision": "dfb94a0d58124ab7b036",
+    "url": "/JobListing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "24140394390cf7b45f40",
+    "url": "/JobListing/static/js/2.a4523b15.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/JobListing/static/js/2.a4523b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb94a0d58124ab7b036",
+    "url": "/JobListing/static/js/main.70353a7c.chunk.js"
+  },
+  {
+    "revision": "52b156b2b8e01fff7d07",
+    "url": "/JobListing/static/js/runtime-main.a8fa18c6.js"
+  }
+]);
